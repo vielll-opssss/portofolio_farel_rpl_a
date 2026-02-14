def0245983e389f4195b4fb6c927f84090180dce
viell-opssss <fareljeremiah973@gmail.com>
2026-02-14 10:07:55 +0700

    Fix import casing: use lowercase for pages/home imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar.jsx";
-import Footer from "./components/footer.jsx";
-import Home from "./pages/home.jsx";
-import About from "./pages/about.jsx";
-import Contact from "./pages/contact.jsx";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
 
 export default function App() {
   return (
